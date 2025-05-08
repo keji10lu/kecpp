75f13f723a6407e9faf219eeaf3ba62e95893ac1
keji10lu <baiwubuzhan@163.com>
2025-05-08 14:32:33 +0800

    第一次测试

---
+++ Project5/main.cpp
@@ -0,0 +1,23 @@
+#include <windows.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string food;
+	string cats[3] = { "ddsd","dwr","dffd" };
+	int count = 0;
+	/*cout << "你喜欢吃什么" << endl;
+	while (cin >> food) {
+		count += 1;
+		cout << "你喜欢吃的第" << count << "个美食是" << food << endl;
+	}
+	cout << "你喜欢吃的美食有" << count << "种" << endl;*/
+	
+	int x = 100;
+	char y = (char)x;
+	cout << "x=" << x << endl;
+	cout << "y=" << y << endl;
+	system("pause");
+	return 0;
+}
